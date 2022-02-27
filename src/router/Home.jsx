292b122975e9eaef4399c340c30beaa42670e571
Luvous <thomasvarela2001@outlook.es>
2022-02-27 17:00:32 -0300

    Problem with fetching api in autocomplete search

--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import NavBar from '../components/NavBar'
+import { Autocomplete } from '@mantine/core';
+import axios from 'axios';
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    const [movieNames, setMovieNames] = useState([])
+    const [searchValue, setSearchValue] = useState("");
+
+    const onChangeHandler = async (text) => {
+        const response = await axios.get(
+            'https://yts.mx/api/v2/list_movies.json?query_term=' + text
+        );
+
+        let arrayOfMoviesNames = [];
+
+        response.data.data.movies.forEach(i => {
+            arrayOfMoviesNames.push(i.title)
+        });
+
+        setMovieNames(arrayOfMoviesNames);
+    }
+
+    console.log(movieNames)
+
+    return (
+        <div>
+            <NavBar />
+            <div className='container'>
+                <Autocomplete
+                    className='autocomplete'
+                    placeholder="Pick one"
+                    limit={8}
+                    data={movieNames}
+                    aria-label="search movie"
+                    value={searchValue}
+                    onChange={(e) => onChangeHandler(e.target.value)}
+
+                    transition="pop-top-left"
+                    transitionDuration={80}
+                    transitionTimingFunction="ease"
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Home
--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,16 @@ import {
 } from 'react-router-dom';
 
 import Home from './Home.jsx';
+import Browse from './Browse.jsx';
+import Movie from './Movie.jsx';
 
 const Routing = () =>{
   return (
     <Router>
         <Routes>
             <Route path='/' element={Home()} />
+            <Route path='/search' element={Browse()}/>
+            <Route path='/movie' element={Movie()} />
         </Routes>
     </Router>
   )
